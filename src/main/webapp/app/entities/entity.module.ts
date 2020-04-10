7d4983dc1c1b4fbe737572bf618535d9a07e3147
thoory <tomtom.ory@gmail.com>
2020-04-10 12:58:57 +0200

    Revert "Entity creation"
    This reverts commit b63c46ed2add49fa7ab8ca983d81d1073c0c2c7e.

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,46 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {
-        path: 'video',
-        loadChildren: () => import('./video/video.module').then(m => m.GuessMyCategoryVideoModule)
-      },
-      {
-        path: 'actor',
-        loadChildren: () => import('./actor/actor.module').then(m => m.GuessMyCategoryActorModule)
-      },
-      {
-        path: 'optional-image',
-        loadChildren: () => import('./optional-image/optional-image.module').then(m => m.GuessMyCategoryOptionalImageModule)
-      },
-      {
-        path: 'keyword',
-        loadChildren: () => import('./keyword/keyword.module').then(m => m.GuessMyCategoryKeywordModule)
-      },
-      {
-        path: 'tag',
-        loadChildren: () => import('./tag/tag.module').then(m => m.GuessMyCategoryTagModule)
-      },
-      {
-        path: 'party',
-        loadChildren: () => import('./party/party.module').then(m => m.GuessMyCategoryPartyModule)
-      },
-      {
-        path: 'player',
-        loadChildren: () => import('./player/player.module').then(m => m.GuessMyCategoryPlayerModule)
-      },
-      {
-        path: 'score',
-        loadChildren: () => import('./score/score.module').then(m => m.GuessMyCategoryScoreModule)
-      },
-      {
-        path: 'answer',
-        loadChildren: () => import('./answer/answer.module').then(m => m.GuessMyCategoryAnswerModule)
-      },
-      {
-        path: 'session',
-        loadChildren: () => import('./session/session.module').then(m => m.GuessMyCategorySessionModule)
-      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
